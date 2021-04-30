98cdbee6b29a239a911fb0c942e215b59439ae62
Sara Gowen <sara.gowen@ukho.gov.uk>
2021-04-30 12:10:16 +0100

    Added Mocha and Chai

---
+++ tests.js
@@ -0,0 +1,8 @@
+var should = require('chai').should()
+
+  describe('Mocha and Chai', function() {
+    it('should work', function() {
+      var whateveryouwant = '10';
+      whateveryouwant.should.equal('10');
+    });
+  });
