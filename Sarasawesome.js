0f56f7be82b03455f735a18ea4b3b8f4e5288c29
asiobhan@Engineering.ukho.gov.uk <asiobhan@Engineering.ukho.gov.uk>
2021-04-30 11:22:32 +0100

    player one and player two created

--- Sarasawesome.js
+++ Sarasawesome.js
@@ -1,8 +1,11 @@
 const { chromium } = require('playwright');
 (async () => {
-    const browser = await chromium.launch({ headless: false, slowMo: 100 });
-    // Create pages, interact with UI elements, assert values
-    const page = await browser.newPage();
-    await page.goto('http://whatsmyuseragent.org/');
+    const browser = await chromium.launch({ headless: false, slowMo: 300 });
+    const playerOneContext = await browser.newContext();
+    const playerTwoContext = await browser.newContext();
+    const pagePlayerOne = await playerOneContext.newPage();
+    const pagePlayerTwo = await playerTwoContext.newPage();
+    await pagePlayerOne.goto('https://studio.boardgamearena.com/');
+    await pagePlayerTwo.goto('https://studio.boardgamearena.com/');
     await browser.close();
 })();
